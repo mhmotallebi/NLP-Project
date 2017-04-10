6e13e7ce1b882cde5c138b03e13557476d30f31b
Mohammad Hossein Motallebi <mhmotallebi@yahoo.com>
2017-04-09 21:38:25 -0600

    added feature to add sentences based on their alignment score and translated words being sequential

--- create_target_trainset.py
+++ create_target_trainset.py
@@ -1,7 +1,19 @@
 import sys
 import re
 
-def generate_target_trainset(english_dict,giza_output_file_name,output_file_name):
+def is_aligned_well_enough(alignment_threshold, alignment_exponent):
+	if alignment_exponent<=alignment_threshold:
+		return True
+	return False
+
+def are_NE_translations_consecutive(word_indices):
+	word_indices = [int(x) for x in word_indices.strip().split()]
+	for number_ind in range(1,len(word_indices)):
+		if word_indices[number_ind]-1!=word_indices[number_ind-1]:
+			return False
+	return True
+
+def generate_target_trainset(english_dict,giza_output_file_name,output_file_name,alignment_threshold,sequential_words):
 	giza_output_file = open(giza_output_file_name,'r')
 	output_file = open(output_file_name,'w')
 	dbg_counter = -1
@@ -22,7 +34,14 @@ def generate_target_trainset(english_dict,giza_output_file_name,output_file_name
 		print('sent_number is:',sent_number)
 		if sent_number not in english_dict: # sentence does not contain any NE, lets skip it!
 			continue
-		
+		try:		
+			alignment_exponent = int(line1.strip().split('e-')[1])
+		except:
+			print(line1)
+			#sys.exit(1)
+			continue
+		if is_aligned_well_enough(alignment_threshold,alignment_exponent)==False:
+			continue
 		target_words_list = line2.split()
 		word_pairs_numbers = re.findall('(?<=\(\{).*?(?=\}\))',line3)
 		if word_pairs_numbers==None or len(word_pairs_numbers)==0:# a word that there is no word in the translated side for. skipping
@@ -30,6 +49,7 @@ def generate_target_trainset(english_dict,giza_output_file_name,output_file_name
 		print(dbg_counter)
 		
 		source_words_list = re.sub('\(\{.*?\}\)','',line3).strip().split()
+		NE_translations_are_consecutive = True
 		for word_ind,word in enumerate(source_words_list):
 			if word =='NULL':
 				print('NULL continued')
@@ -38,10 +58,16 @@ def generate_target_trainset(english_dict,giza_output_file_name,output_file_name
 				print(word_ind,len(word_pairs_numbers),line3)
 				
 				temp = word_pairs_numbers[word_ind].strip()
+				if sequential_words:# make sure words are consecutive in the translation side for each NE
+					NE_translations_are_consecutive = are_NE_translations_consecutive(temp)	
+					if NE_translations_are_consecutive==False:
+						break			
 				if len(temp)==0:
 					continue
 				for number in word_pairs_numbers[word_ind].strip().split():
 					target_words_list[int(number)-1] = [target_words_list[int(number)-1],english_dict[sent_number][word.lower()]]
+		if NE_translations_are_consecutive==False:
+			continue
 		print(len(target_words_list))
 		
 		for word_part in target_words_list:
@@ -114,11 +140,13 @@ def load_english_tagged_sentences(tagged_sentences_file_name):
 def main():
 	english_tagged_file_name = '../Extracted English NEs/lists-conll2003/out-v4.final'
 	giza_output_file_name = '../alignment/parallel_corpus/output.en_fa.dict.A3.final'
-	output_file_name = 'out.2'
+	output_file_name = 'out.4'
+	alignment_threshold = 40# To keep more similar sentences
+	sequential_words = True # if an English NE is aligned with n Target Language Words, these words should be consecutive
 	english_tagged_dict = load_english_tagged_sentences(english_tagged_file_name)
 	#for key,val in english_tagged_dict.items():
 	#	print(key,val)
-	generate_target_trainset(english_tagged_dict,giza_output_file_name,output_file_name)
+	generate_target_trainset(english_tagged_dict,giza_output_file_name,output_file_name,alignment_threshold,sequential_words)
 
 if __name__ == "__main__":
 	main()
